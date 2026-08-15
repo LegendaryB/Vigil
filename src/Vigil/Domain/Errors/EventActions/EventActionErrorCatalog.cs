2e10c042d3482190f36787dbb99db024b474c5a2
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2026-08-15 02:40:20 +0200

    .

--- src/Vigil/Domain/Errors/EventActions/EventActionErrorCatalog.cs
+++ src/Vigil/Domain/Errors/EventActions/EventActionErrorCatalog.cs
@@ -6,6 +6,10 @@ internal sealed class EventActionErrorCatalog : DomainErrorCatalog
 {
     protected override string Prefix => "event_action_";
 
+    internal string InvalidPriority => Prefix + "invalid_priority";
+
     internal static string EventActionNotFoundMessage(Guid id) =>
         EntityNotFoundMessage(nameof(EventAction), id);
+
+    internal const string InvalidPriorityMessage = "Priority must be at least 1.";
 }
